35005fd38591e4c3769c70f9257b07315179eac7
spondbob <spondbob@eamca.com>
2014-01-16 13:22:29 +0700

    fix models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment
   include Mongoid::Document
+  field :name,                 :type => String
+  field :email,                :type => String
+  field :content,              :type => String
+  belongs_to :post,            :class_name => 'Post'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post
   include Mongoid::Document
+  field :title,                 :type => String
+  field :content,               :type => String
+  has_many :comments,           :class_name => 'Comment'
 end
